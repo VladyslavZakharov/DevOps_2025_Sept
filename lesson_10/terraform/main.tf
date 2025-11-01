e836b00c4c21d7b9064f8091251914e41be0c8dc
Vlad Zakharov <vladzakharov339@gmail.com>
2025-10-31 19:36:49 -0700

    12

--- lesson_10/terraform/main.tf
+++ lesson_10/terraform/main.tf
@@ -60,14 +60,14 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+  ngress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+  ngress {
     from_port   = 22
     to_port     = 9001
     protocol    = "tcp"
